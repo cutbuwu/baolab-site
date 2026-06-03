0a1cd44962edc8daf8f023a823aca726f633f680
root <root@BigCutbuwu.localdomain>
2026-06-03 19:24:53 +1000

    restore: film grain and custom cursor (z-index fixed)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Header from "./Header";
+import Cursor from "./components/Cursor";
 
 
 export const metadata: Metadata = {
@@ -54,6 +55,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
 
+        <Cursor />
         <Header />
 
         <main>{children}</main>
